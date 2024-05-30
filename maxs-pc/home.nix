651d3cb44526351abceb0a6de925b0cc5aad6541
Maxwell Morris <git@maxmorris.io>
2024-05-30 20:20:56 +0100

    Switching to NixOS which deprecates... everything.

---
+++ maxs-pc/home.nix
@@ -0,0 +1,63 @@
+{ config, lib, pkgs, ... }:
+let
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-23.11.tar.gz";
+in
+{
+  imports = [
+    (import "${home-manager}/nixos")
+  ];
+
+  users.users.max = {
+	shell = pkgs.zsh;
+	ignoreShellProgramCheck = true;
+    isNormalUser = true;
+  };
+
+  fonts.packages = with pkgs; [
+    font-awesome
+    powerline-fonts
+    powerline-symbols
+    ubuntu_font_family
+  ];
+
+  home-manager = {
+    useGlobalPkgs = true;
+    users.max = { 
+    pkgs, ... }: {
+        home.stateVersion = "23.11";
+        home.packages = [ 
+          pkgs.zsh
+          pkgs.vim
+          pkgs.speedtest-cli
+          pkgs.fastfetch
+          pkgs.btop
+          pkgs.git
+          pkgs._1password
+        ];
+
+    programs = {
+      zsh = {
+        enable = true;
+        syntaxHighlighting.enable = true;
+        enableAutosuggestions = true;
+        initExtra = "touch .zshrc 2> /dev/null ; if [ $TTY = \"/dev/tty1\" ] && [[ $HOST = maxs-pc || $HOST = maxs-laptop ]] ; then ; startx; exit ;fi";
+      };
+      zsh.oh-my-zsh = {
+        enable = true;
+        theme = "jonathan";
+      };
+      git = {
+        enable = true;
+        userName = "Maxwell Morris";
+        userEmail = "git@maxmorris.io";
+      };
+    };
+
+    home.sessionVariables = {
+      EDITOR = "vim";
+      SUDO_PROMPT = "[⚡ sudo on $(cat /etc/hostname) for $USER]:";
+    };
+
+    };
+  };
+}
