5d335ce1a0233a3b68d9dad50cb9f8ec05160b7b
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2024-05-30 23:30:33 +0100

    Update install-config.sh

--- install-config.sh
+++ install-config.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
 
-if [[ -f $(cat /etc/hostname) ]]; then
-  ln -s ./$(cat /etc/hostname)/configuration.nix /etc/nixos/configuration.nix
+CONFIG_PATH="./$(cat /etc/hostname)/configuration.nix"
+
+if [ -f ${CONFIG_PATH} ]; then
+  ln -s ${CONFIG_PATH} /etc/nixos/configuration.nix
   echo "OK - Symlinked configuration.nix for $(cat /etc/hostname)"
 else
   echo "FAIL - Unable to locate a configuration.nix for $(cat /etc/hostname)"
