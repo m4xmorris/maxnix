faf5b124ff69000fa576a2a5b6f6ea980101a1db
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2024-05-31 00:14:28 +0100

    Create configuration.nix

---
+++ keyboard-pi/configuration.nix
@@ -0,0 +1,35 @@
+{ config, lib, pkgs, ... }:
+{
+  imports =
+    [
+      ../modules/update-config.nix
+      ./hardware.nix
+      ../modules/home.nix
+      ../modules/i3.nix
+    ];
+
+  nixpkgs.config.allowUnfree = true;
+
+  users.users.max.extraGroups = [ "wheel" ];
+
+  networking.hostName = "maxs-laptop";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Europe/London";
+
+  i18n.defaultLocale = "en_GB.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = lib.mkForce "us";
+    useXkbConfig = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    wget
+    btop
+    killall
+    moonlight-qt
+  ];
+
+  system.stateVersion = "23.11";
+}
