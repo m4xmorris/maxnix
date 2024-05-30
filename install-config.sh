f797fcc7e154d3c052dccdb94c1f3c780ad4b1a3
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2024-05-30 23:37:37 +0100

    Update install-config.sh

--- install-config.sh
+++ install-config.sh
@@ -6,6 +6,12 @@ CONFIG_PATH="/etc/nixos/$(cat /etc/hostname)/configuration.nix"
 if [ -f ${CONFIG_PATH} ]; then
   ln -s ${CONFIG_PATH} /etc/nixos/configuration.nix
   echo "OK - Symlinked configuration.nix for $(cat /etc/hostname)"
+  read -p "Rebuild now (N/switch/boot)?" REBUILD
+  if [ ${REBUILD} == "switch" ];then
+    nixos-rebuild switch
+  elif [ ${REBUILD} == "boot" ]; then
+    nixos-rebuild boot
+  fi
 else
   echo "FAIL - Unable to locate a configuration.nix for $(cat /etc/hostname)"
 fi
