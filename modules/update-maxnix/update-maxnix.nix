830e3dd15bdb9f5d14bfde1b5222137b69d1d011
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2024-05-31 16:07:36 +0100

    Update update-maxnix.nix

--- modules/update-maxnix/update-maxnix.nix
+++ modules/update-maxnix/update-maxnix.nix
@@ -2,6 +2,6 @@
 {
   environment = {
     systemPackages = with pkgs; [git];
-    shellAliases = { update-config = "sudo rm -r /etc/nixos && sudo git clone https://github.com/m4xmorris/maxnix /etc/nixos && sudo bash /etc/nixos/install-config.sh"; };
+    shellAliases = { update-maxnix = "sudo rm -r /etc/nixos && sudo git clone https://github.com/m4xmorris/maxnix /etc/nixos && sudo bash /etc/nixos/modules/update-maxnix/install-config.sh"; };
   };
 }
